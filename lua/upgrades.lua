17071acbe7f6f0dbfd35bea8e178f8d3f5f25da3
Doridian <mriq91@gmail.com>
2017-08-29 19:19:13 -0700

    comments

--- lua/upgrades.lua
+++ lua/upgrades.lua
@@ -1,6 +1,7 @@
 -- db.upgrades store owner, can be updated atomically by serial
 -- db.user stores array of serials that gets auto-fixed on every op
 
+-- FIRST we find upgrades affected, LAST we insert the log
 -- changes by position get serials from user, then push atomic updates by serial (race condition, so script access by serial preferred!)
 -- changes by serial just update atomically
 
