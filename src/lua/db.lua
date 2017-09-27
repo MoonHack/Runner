ec8da522338b829b94c16ffa324113e80e926dcc
Doridian <mriq91@gmail.com>
2017-09-27 07:11:57 -0700

    add custom tojson stuff

--- src/lua/db.lua
+++ src/lua/db.lua
@@ -4,6 +4,45 @@ local clientCore = mongo.Client(config.core)
 local clientUsers = mongo.Client(config.users)
 local time = os.time
 local tinsert = table.insert
+local dgetmetatable = debug.getmetatable
+local dsetmetatable = debug.setmetatable
+local json_quote = require("dkjson").quotestring
+
+local function simple_tostr__tojson(pref, suff)
+	suff = suff or '"}'
+	return function(self)
+		return pref .. tostring(self) .. suff
+	end
+end
+local function simple_unpack__tojson(pref, suff)
+	suff = suff or '"}'
+	return function(self)
+		return pref .. self:unpack() .. suff
+	end
+end
+local function make__tojson(struct, func)
+	local mt = dgetmetatable(struct)
+	mt.__tojson = func
+	table.foreach(mt, print)
+	dsetmetatable(struct, mt)
+end
+local function make_simple_tostr__tojson(struct, pref, suff)
+	make__tojson(struct, simple_tostr__tojson(pref, suff))
+end
+local function make_simple_unpack__tojson(struct, pref, suff)
+	make__tojson(struct, simple_unpack__tojson(pref, suff))
+end
+
+make_simple_tostr__tojson(mongo.ObjectId, '{"$oid":"')
+make_simple_unpack__tojson(mongo.DateTime, '{"$date":{"$numberLong": ', '"}}')
+make__tojson(mongo.Timestamp, function(self)
+	local t, i = self:unpack()
+	return '{"$timestamp":{"t":'..t..',"i":'..i..'}}'
+end)
+make__tojson(mongo.Regex, function(self)
+	local re, opt = self:unpack()
+	return '{"$regex":'..json_quote(re)..',"$options":'..json_quote(opt)..'}'
+end)
 
 local function now()
 	return time() * 1000
