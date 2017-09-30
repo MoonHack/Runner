5cf0790483bb320c5b686c6f5a64c4e400f762c6
Doridian <mriq91@gmail.com>
2017-09-30 13:19:33 -0700

    randomtest

---
+++ tests/random.py
@@ -0,0 +1,8 @@
+from tests.__classes__ import BaseTest
+
+test = BaseTest('Randomness check')
+test.create_script('local x = math.random(); math.randomseed(0); local a1 = math.random(); math.randomseed(0); local a2 = math.random(); return x == a1, x == a2, a1 == a2;')
+
+test.new_execution('Ensure stuff works')
+test.expect_return([False, False, True])
+test.expect_ok()
