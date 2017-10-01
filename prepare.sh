9d3eecfaf2ff2ddaea47d563ffd61fd70d9c7e64
Doridian <mriq91@gmail.com>
2017-10-01 07:09:10 -0700

    true

--- prepare.sh
+++ prepare.sh
@@ -2,8 +2,11 @@
 set -e
 if [ ! -d "/sys/fs/cgroup/memory/$USER" ];
 then
-	U="$USER"
-	sudo mkdir -p "/sys/fs/cgroup/memory/$U" || true
-	sudo chown -R "$U:$U" "/sys/fs/cgroup/memory/$U" || true
+	if [ -d "/sys/fs/cgroup/memory" ];
+	then
+		U="$USER"
+		sudo mkdir -p "/sys/fs/cgroup/memory/$U"
+		sudo chown -R "$U:$U" "/sys/fs/cgroup/memory/$U"
+	fi
 fi
 rmdir "/sys/fs/cgroup/memory/$USER/moonhack_cg_"* || true
