39558149b6d49f819a6bb7f68d24e5a3e3264830
Mark Dietzer <mardie@microsoft.com>
2018-11-08 17:56:33 +0100

    more devconf

--- devconf.sh
+++ devconf.sh
@@ -2,6 +2,10 @@ export RMQ_HOST="127.0.0.1"
 export RMQ_PORT=5672
 export RMQ_USER="guest"
 export RMQ_PASS="guest"
+export RMQ_URL="amqp://$RMQ_USER:$RMQ_PASS@$RMQ_HOST:$RMQ_PORT/"
+
 export MONGODB_CORE="mongodb://127.0.0.1/moonhack_core"
 export MONGODB_USERS="mongodb://127.0.0.1/moonhack_users"
 
+export PORT=3000
+export SESSION_SECRET="meow"
