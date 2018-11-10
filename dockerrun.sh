3fcb571c65031df5cffcb370d7cb31e4dca93386
Mark Dietzer <mardie@microsoft.com>
2018-11-10 02:28:53 +0100

    only mount subdir of cgroup

--- dockerrun.sh
+++ dockerrun.sh
@@ -6,10 +6,7 @@ mkdir -p /opt/runnertmp/root
 export U="runner"
 export UID="$(id -u "$U")"
 
-export CGROUPDIR="$(hostname)"
-
-mkdir -p "/sys/fs/cgroup/memory/$CGROUPDIR" || true
-chown -R "$U:$U" "/sys/fs/cgroup/memory/$CGROUPDIR" || true
+chown -R "$U:$U" "$CGROUPDIR"
 
 exec ./simple_master "$RUNNER_COUNT" "$UID"
 
--- src/c/worker.c
+++ src/c/worker.c
@@ -200,7 +200,7 @@ static void cgroup_init() {
 	}
 
 	char cgroup_mem_root[200];
-	sprintf(cgroup_mem_root, "/sys/fs/cgroup/memory/%s/moonhack_cg_%d/", getenv("CGROUPDIR"), getpid());
+	sprintf(cgroup_mem_root, "%s/moonhack_cg_%d/", getenv("CGROUPDIR"), getpid());
 	sprintf(cgroup_mem_limit, "%smemory.limit_in_bytes", cgroup_mem_root);
 	sprintf(cgroup_memsw_limit, "%smemory.memsw.limit_in_bytes", cgroup_mem_root);
 	sprintf(cgroup_mem_tasks, "%stasks", cgroup_mem_root);
